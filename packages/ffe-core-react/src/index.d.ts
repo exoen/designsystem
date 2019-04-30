11b8b12e60ded629d27e86f15f0e31d15793c56a
Hege Daljord Morken <hege.daljord.morken@sparebank1.no>
2019-04-30 15:15:02 +0200

    feat(ffe-core-react): Added type definitions for typescript

---
+++ packages/ffe-core-react/src/index.d.ts
@@ -0,0 +1,71 @@
+import * as React from 'react';
+
+export interface DividerLineProps extends React.HTMLProps<HTMLHRElement> {
+    className?: string;
+}
+
+export interface EmphasizedTextProps extends React.HTMLProps<HTMLElement> {
+    children: React.ReactNode;
+    className?: string;
+}
+
+export interface HeadingProps extends React.HTMLProps<HTMLHeadingElement> {
+    children: React.ReactNode;
+    className?: string;
+    error?: boolean;
+    inline?: boolean;
+    lookLike?: 1 | 2 | 3 | 4 | 5 | 6;
+    noMargin?: boolean;
+    withBorder?: boolean;
+}
+
+export interface LinkTextProps extends React.HTMLProps<HTMLElement> {
+    children: React.ReactNode;
+    className?: string;
+    element?: HTMLElement | string;
+    underline?: boolean;
+}
+
+export interface SimpleElementProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+export interface ParagraphProps extends React.HTMLProps<HTMLParagraphElement> {
+    children: React.ReactNode;
+    className?: string;
+    lead?: boolean;
+    subLead?: boolean;
+    textCenter?: boolean;
+    textLeft?: boolean;
+}
+
+declare class DividerLine extends React.Component<DividerLineProps, any> {}
+declare class EmphasizedText extends React.Component<
+    EmphasizedTextProps,
+    any
+> {}
+declare class Heading1 extends React.Component<HeadingProps, any> {}
+declare class Heading2 extends React.Component<HeadingProps, any> {}
+declare class Heading3 extends React.Component<HeadingProps, any> {}
+declare class Heading4 extends React.Component<HeadingProps, any> {}
+declare class Heading5 extends React.Component<HeadingProps, any> {}
+declare class Heading6 extends React.Component<HeadingProps, any> {}
+declare class LinkText extends React.Component<LinkTextProps, any> {}
+declare class MicroText extends React.Component<
+    SimpleElementProps & React.HTMLProps<HTMLSpanElement>,
+    any
+> {}
+declare class Paragraph extends React.Component<ParagraphProps, any> {}
+declare class PreformattedText extends React.Component<
+    SimpleElementProps & React.HTMLProps<HTMLPreElement>,
+    any
+> {}
+declare class SmallText extends React.Component<
+    SimpleElementProps & React.HTMLProps<HTMLSpanElement>,
+    any
+> {}
+declare class StrongText extends React.Component<
+    SimpleElementProps & React.HTMLProps<HTMLElement>,
+    any
+> {}
